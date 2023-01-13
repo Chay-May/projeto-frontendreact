24d5c74d86541a7de22caeefaa86eb1569ef3bae
Chay-May <chayenne.mayara@gmail.com>
2023-01-12 21:51:45 -0300

    atualizacao imagens png

--- src/Componets/Cards/index.jsx
+++ src/Componets/Cards/index.jsx
@@ -1,12 +1,12 @@
 import { StyleButton, StyleCard, StyleImagem, StyleInfo } from "./style";
 
-function Cards() {
+function Cards(propsc) {
   return (
   <StyleCard>
     <StyleInfo>
-        <StyleImagem src="https://brandili.vteximg.com.br/arquivos/ids/273753-484-670/image-f92a45331d4a480785007422b03bdca8.jpg?v=637783053894130000" alt="Brinquedo galinha" />
-        <p>Chocalho girassol</p>
-        <p>R$ 10,50</p>
+        <StyleImagem src="src/imagens/Imagem do WhatsApp de 2023-01-12 à(s) 20.39.39.jpg" alt="imagem" />
+        <p>{props.nome}</p>
+        <p>{props.valor}</p>
         <StyleButton>ADD CARRINHO</StyleButton>
     </StyleInfo>
   </StyleCard>
